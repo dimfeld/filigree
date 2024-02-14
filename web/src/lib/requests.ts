b5e5f382b6255151c0c5ecc32d0f9a918a401855
Daniel Imfeld <daniel@imfeld.dev>
2024-02-14 09:24:11 -1000

    add more form helpers

--- web/src/lib/requests.ts
+++ web/src/lib/requests.ts
@@ -1,4 +1,11 @@
-import { error, fail, type Cookies, type NumericRange, type RequestEvent } from '@sveltejs/kit';
+import {
+  error,
+  fail,
+  type Cookies,
+  type NumericRange,
+  type RequestEvent,
+  type ActionFailure,
+} from '@sveltejs/kit';
 import { client, type Client, type HttpMethod, type RequestOptions } from './client.js';
 import parseCookie from 'set-cookie-parser';
 import type { FormResponse } from './forms.svelte.js';
@@ -37,30 +44,57 @@ export interface ErrorResponse<KIND extends string = string, DETAILS extends obj
   error: ErrorField<KIND, DETAILS>;
 }
 
+/** Determine if a response from the API is an error of a particular kind */
 export function isErrorResponse<T extends ErrorResponse<string, object>>(
   obj: object | null | undefined,
-  kind: string
+  kind?: string
 ): obj is T {
   if (!obj) {
     return false;
   }
-  return obj && 'error' in obj && (obj.error as T['error'])?.kind === kind;
+
+  if (kind) {
+    return obj && 'error' in obj && (obj.error as T['error'])?.kind === kind;
+  } else {
+    return obj && 'error' in obj;
+  }
 }
 
-export async function forwardFormToApi<T extends object>(
-  method: HttpMethod,
-  url: string,
-  event: RequestEvent,
-  options?: Partial<RequestOptions> & { client?: Client }
-): Promise<FormResponse<T>> {
-  const response = await forwardToApi(method, url, event, {
-    tolerateFailure: true,
-    ...options,
-  });
+/** Data extracted from a response */
+export interface ExtractedResponse<T> {
+  type: 'extracted-response';
+  status: number;
+  body: T;
+  headers: Headers;
+}
 
-  // todo handle 403?
-  if (response.status === 400) {
-    fail(400, await response.json());
+export function isExtractedResponse<T>(
+  t: ExtractedResponse<T> | ActionFailure<T>
+): t is ExtractedResponse<T> {
+  return t && 'type' in t && t.type === 'extracted-response';
+}
+
+/** Given the return value of handleFormResponse, return either the body or the ActionFailure */
+export function getReturnBody<T>(
+  value: ActionFailure<FormResponse<T>> | ExtractedResponse<FormResponse<T>>
+) {
+  if (isExtractedResponse(value)) {
+    return value.body;
+  } else {
+    return value;
+  }
+}
+
+/** Handle a form response, returning an ActionFailure if the status code matches validationFailureCodes,
+ * throwing an error if the status code is some other failure code, or returning the body otherwise. */
+export async function handleFormResponse<T extends object>(
+  response: Response,
+  validationFailureCodes: number[] = [400]
+): Promise<ActionFailure<FormResponse<T>> | ExtractedResponse<FormResponse<T>>> {
+  let { headers, status } = response;
+
+  if (validationFailureCodes.includes(response.status)) {
+    return fail<FormResponse<T>>(response.status, await response.json());
   } else if (!response.ok) {
     let err: object;
     try {
@@ -72,7 +106,39 @@ export async function forwardFormToApi<T extends object>(
     error(response.status as NumericRange<400, 599>, err);
   }
 
-  return await response.json();
+  const body: FormResponse<T> = await response.json();
+
+  return {
+    status: 'extracted-response',
+    body,
+    headers,
+    code: status,
+  };
+}
+
+/** Forward a form request to an API endpoint, and check the response for errors and validation failures.
+ * This is designed to be a "do-everything" function for standard form actions. More complex needs can be met by
+ * calling `forwardToApi`, `handleFormResponse`, and `getReturnBody` as needed.
+ *
+ * @param method The HTTP method to use.
+ * @param url The URL of the API endpoint. This function adds the `/api/` prefix.
+ * @param event The event supplied by SvelteKit.
+ * @param options Additional options for this request, which are passed to the client. Pass validationFailureCodes to override the default value of `[400]`.
+ * */
+export async function forwardFormToApi<T extends object>(
+  method: HttpMethod,
+  url: string,
+  event: RequestEvent,
+  options?: Partial<RequestOptions> & { client?: Client; validationFailureCodes?: number[] }
+): Promise<ActionFailure<FormResponse<T>> | FormResponse<T>> {
+  const response = await forwardToApi(method, url, event, {
+    tolerateFailure: true,
+    ...options,
+  });
+
+  const validationFailureCodes = options?.validationFailureCodes ?? [400];
+  const r = await handleFormResponse<T>(response, validationFailureCodes);
+  return getReturnBody(r);
 }
 
 /** Copy the Set-Cookie headers from the given response into the given headers. */
